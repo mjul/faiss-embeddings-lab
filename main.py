08fc851a3ec56da5cb899aa074d67be9dd3a4289
Martin Jul <martin@mjul.com>
2023-05-09 09:48:15 +0200

    Ignore PDFs with errors while parsing out text

--- main.py
+++ main.py
@@ -30,7 +30,17 @@ def extract_text_from_pdf(file):
     :param file: the path to the file
     :return: the text
     """
-    reader = PyPDF2.PdfReader(file)
+    try:
+        reader = PyPDF2.PdfReader(file)
+    except PyPDF2.errors.PdfReadError:
+        print(f"** Could not read PDF file: {file}  (PdfReadError)")
+        return ""
+    except PyPDF2.errors.DependencyError:
+        print(f"** Could not read PDF file: {file}  (DependencyError)")
+        return ""
+    except Exception:
+        print(f"** Could not read PDF file: {file}  (Exception)")
+        return ""
     text = ""
     for page in reader.pages:
         text += page.extract_text() + "\n"
@@ -48,31 +58,49 @@ def get_embeddings(text_list):
     return model_output.last_hidden_state[:, 0, :].detach().cpu().numpy()
 
 
-def main():
-    print("Searching for PDF files...")
-    pdf_files = [p for p in DOCUMENT_DIR.iterdir()
-                 if p.suffix == ".pdf" and p.is_file()]
+def parse_pdf_files(pdf_files):
+    print("Parsing PDF files...")
+    file_texts = []
+    for i, file in enumerate(pdf_files):
+        if i % 25 == 0:
+            print(f"  Parsing file {i} of {len(pdf_files)}: {file}")
+        file_texts.append(extract_text_from_pdf(file))
+    return file_texts
 
+
+def create_index(pdf_files):
+    """Parse the PDF files and create the FAISS index."""
+    file_texts = parse_pdf_files(pdf_files)
+
+    print("Computing embeddings...")
+    embeddings = get_embeddings(file_texts)
+    print("Creating index...")
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(np.ascontiguousarray(embeddings))
+    print("Saving index...")
+    faiss.write_index(index, str(INDEX_FILE))
+
+
+def conditionally_create_or_update_index(pdf_files):
+    """Create or update the index if any of the files are newer than the index."""
     newest_pdf = max(pdf_files, key=lambda p: p.stat().st_mtime)
     newest_pdf_timestamp = newest_pdf.stat().st_mtime
-
     # Rebuild index if necessary
     is_index_up_to_date = INDEX_FILE.exists() and INDEX_FILE.stat().st_mtime > newest_pdf_timestamp
     if is_index_up_to_date:
         print("Index is up-to-date.")
     else:
-        print("Parsing PDF files...")
-        file_texts = [extract_text_from_pdf(file) for file in pdf_files]
+        create_index(pdf_files)
 
-        print("Computing embeddings...")
-        embeddings = get_embeddings(file_texts)
 
-        print("Creating index...")
-        index = faiss.IndexFlatL2(embeddings.shape[1])
-        index.add(np.ascontiguousarray(embeddings))
+def main():
+    print("Searching for PDF files...")
+    pdf_files = [p for p in DOCUMENT_DIR.iterdir()
+                 if p.suffix == ".pdf" and p.is_file()]
 
-        print("Saving index...")
-        faiss.write_index(index, str(INDEX_FILE))
+    pdf_files = pdf_files[:100] # TODO: remove this, for testing only
+
+    conditionally_create_or_update_index(pdf_files)
 
     print("Loading index...")
     index = faiss.read_index(str(INDEX_FILE))
